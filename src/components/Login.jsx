833e06f18448b47ae73f2f7e54ff3b9cb230fd21
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-10-31 18:15:39 +0530

    SignIn form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div className="bg-amber-500 w-full h-screen flex items-center justify-center">
-      <div className="bg-red-500 w-80 h-100">
-        <div className="text-center mt-2">
-          <h1 className="text-3xl font-medium font-serif mb-3">
-            Login to Netflix-GPT
-          </h1>
-          <hr />
-        </div>
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/37160e4c-a26b-459c-a83e-d7e972e2aea1/web/IN-en-20251027-TRIFECTA-perspective_f563e160-0cc1-4cb1-9c72-2fc41aae986e_large.jpg"
+          alt="bg image"
+        />
       </div>
+      <form className="w-3/12 p-12 absolute my-32 bg-black/80 text-amber-50 mx-auto left-0 right-0 rounded-lg ">
+        <h1 className="text-3xl mb-5 font-semibold">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address or Mobile number"
+          className="p-3 my-4 w-full border border-white bg-gray-700"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-3 my-4 w-full border border-white bg-gray-700"
+        />
+        <button className="w-full bg-rose-700 p-2.5 my-4 rounded-lg">
+          Submit
+        </button>
+        <p className="text-gray-400 text-sm mt-4 cursor-pointer hover:underline">
+          New to Netflix-gpt?{" "}
+          <span className="text-white font-medium mx-1">Sign up now.</span>
+        </p>
+      </form>
     </div>
   );
 };
